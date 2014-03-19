200a9e8e461b16b4fe2fbffa784f6a0e365cbbe0
Benne <benediktschmidt89@gmail.com>
2014-03-19 21:08:08 +0100

    Ah ja, kann sein, dass die DB nicht mehr geht, weil ich den rawPrice rausgekillt hab. In dem Fall musst du die Datenbank einmal händig platt machen. Das geht entweder indem du die komplette DB (dogePizzaDB) einmal killst und wieder neu anlegst, oder versuchst die SQLs einzuspielen

---
+++ dogePizza/database/DROP ALL.sql
@@ -0,0 +1,13 @@
+DROP TABLE meal_ingredient;
+DROP TABLE meal_size;
+DROP TABLE size;
+DROP TABLE question_answer;
+DROP TABLE answer;
+DROP TABLE question;
+DROP TABLE dogepizzauser_meal;
+DROP TABLE dogepizzauser;
+DROP TABLE address;
+DROP TABLE ingredient;
+DROP TABLE menu;
+DROP TABLE meal;
+
